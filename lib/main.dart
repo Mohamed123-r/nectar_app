8c9933912174e70ad769132d8fc3882a2f00ec95
mohamed <mezoattalla123@gmail.com>
2024-03-19 14:41:45 +0200

    create home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/home/presentation/views/home_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-     debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
     );
   }
 }
-
-
