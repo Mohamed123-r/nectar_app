eac8b2cbf812689cc0940ba79895e2c7e3b3f374
mohamed <mezoattalla123@gmail.com>
2024-03-23 23:50:48 +0200

    Refactor HomeView to use separate widget for the body content.

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,66 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:nectar/features/home/presentation/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (fff, ddfd) {
-        return <Widget>[
-          SliverAppBar(
-            floating: true,
-            pinned: true,
-            backgroundColor: Colors.white,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(64.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(
-                          Icons.search,
-                        ),
-                        hintText: 'Search Store',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            elevation: 0,
-            centerTitle: true,
-            title: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/logo.png",
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.location_on),
-                    Text(
-                      "Dhaka, Banassre",
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ];
-      },
-      body: Container(),
-    );
+    return const HomeViewBody();
   }
 }
+
+
