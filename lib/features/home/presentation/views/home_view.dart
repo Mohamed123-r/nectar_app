cdeb55d2bfbe76f110f7ce091613f0325a527936
mohamed <mezoattalla123@gmail.com>
2024-03-23 23:29:58 +0200

    Refactor home_view.dart to include a NestedScrollView with a SliverAppBar containing search functionality and location display. Update pubspec.yaml to include assets/images/ folder.

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -5,6 +5,62 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return NestedScrollView(
+      headerSliverBuilder: (fff, ddfd) {
+        return <Widget>[
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            backgroundColor: Colors.white,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(64.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.search,
+                        ),
+                        hintText: 'Search Store',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            elevation: 0,
+            centerTitle: true,
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo.png",
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.location_on),
+                    Text(
+                      "Dhaka, Banassre",
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ];
+      },
+      body: Container(),
+    );
   }
 }
