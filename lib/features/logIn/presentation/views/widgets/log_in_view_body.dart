84452a400ae244395ef5f1d30ad27084c4caa409
mohamed <mezoattalla123@gmail.com>
2024-03-30 22:37:57 +0200

    Refactored widget heights to use screen util plugin for responsive design.

--- lib/features/logIn/presentation/views/widgets/log_in_view_body.dart
+++ lib/features/logIn/presentation/views/widgets/log_in_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectar/constants.dart';
+import 'package:nectar/core/utils/assets.dart';
 import 'package:nectar/core/widgets/custom_button.dart';
 import 'package:nectar/core/widgets/custom_image_logo.dart';
 import 'package:nectar/features/logIn/presentation/views/widgets/section_text_field.dart';
@@ -13,58 +14,67 @@ class LogInViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding:  EdgeInsets.all(16.0.sp),
-        child: Column(
-          children: [
-             SizedBox(height: 70.sp),
-            const CustomImageLogo(),
-             const SizedBox(height: 100),
-            const SectionTextField(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Forgot Password?",
-                    style: Styles.textStyle14.copyWith(
-                     color: Colors.black
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(Assets.background),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:  EdgeInsets.all(16.0.sp),
+          child: Column(
+            children: [
+               SizedBox(height: 70.sp),
+              const CustomImageLogo(),
+               const SizedBox(height: 100),
+              const SectionTextField(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgot Password?",
+                      style: Styles.textStyle14.copyWith(
+                       color: Colors.black
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-             SizedBox(height: 20.sp),
-            CustomButton(
-              text: "Log In",
-              onPressed: () {},
-            ),
-             SizedBox(height: 25.sp),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account?",
-                  style: Styles.textStyle14.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Sign Up",
+                ],
+              ),
+               SizedBox(height: 20.sp),
+              CustomButton(
+                text: "Log In",
+                onPressed: () {},
+              ),
+               SizedBox(height: 25.sp),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
                     style: Styles.textStyle14.copyWith(
-                      color: kPrimaryColor,
+                      color: Colors.black,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: Styles.textStyle14.copyWith(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
