d4ecf676ac34763d02dcdd16b9b0c28097f1fb31
mohamed <mezoattalla123@gmail.com>
2024-03-31 17:26:45 +0200

    Implemented UI for the forgot password view with necessary text fields and buttons for user input and verification.

--- lib/features/logIn/presentation/views/forget_password_view.dart
+++ lib/features/logIn/presentation/views/forget_password_view.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/utils/assets.dart';
+import 'package:nectar/core/utils/styles.dart';
+import 'package:nectar/core/widgets/custom_button.dart';
+import 'package:nectar/core/widgets/custom_image_logo.dart';
+import 'package:nectar/core/widgets/custom_text_field.dart';
+import 'package:nectar/core/widgets/custom_text_from_text_field.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.background),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0.sp),
+            child: Column(
+              children: [
+                SizedBox(height: 70.sp),
+                const CustomImageLogo(),
+                const SizedBox(height: 100),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Forgot Password",
+                      style: Styles.textStyle26,
+                    ),
+                    SizedBox(height: 15.sp),
+                    const CustomTextFromTextField(
+                        hintText: "Enter your email for verification"
+                            " process we will send 5 digits code"
+                            " to your email"),
+                    SizedBox(height: 40.sp),
+                    const CustomTextFromTextField(
+                      hintText: 'Email',
+                    ),
+                    CustomTextField(
+                      onSaved: (value) {},
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 60.sp),
+                CustomButton(
+                  text: "Continue",
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
