9ff9607b3f24744276aa8f01712a247ec9732683
mohamed <mezoattalla123@gmail.com>
2024-04-02 16:33:45 +0200

    Refactor reset password screen to use FinalShapeFromOPT widget for OTP input field styling. Updated SectionTextFieldFromResetPassword and added FinalShapeFromOPT widget.

---
+++ lib/features/logIn/presentation/views/widgets/final_shape_from_opt.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/constants.dart';
+
+import 'custom_item_from_opt.dart';
+
+class FinalShapeFromOPT extends StatelessWidget {
+  const FinalShapeFromOPT({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 67.sp,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        border: Border.all(color: kPrimaryColor),
+        borderRadius: BorderRadius.circular(15.sp),
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomItemFromOPT(),
+          CustomItemFromOPT(),
+          CustomItemFromOPT(),
+          CustomItemFromOPT(),
+          CustomItemFromOPT(),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/features/logIn/presentation/views/widgets/section_text_field_from_reset_password.dart
+++ lib/features/logIn/presentation/views/widgets/section_text_field_from_reset_password.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nectar/constants.dart';
 import 'package:nectar/core/widgets/custom_text_from_text_field.dart';
 import 'package:nectar/core/widgets/password_text_field.dart';
-
-import 'custom_item_from_opt.dart';
+import 'final_shape_from_opt.dart';
 
 class SectionTextFieldFromResetPassword extends StatelessWidget {
   const SectionTextFieldFromResetPassword({super.key});
@@ -18,20 +16,7 @@ class SectionTextFieldFromResetPassword extends StatelessWidget {
           hintText: 'Enter the 5 digits code',
         ),
         SizedBox(height: 20.sp),
-        Container(
-          height: 67.sp,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            border: Border.all(color: kPrimaryColor),
-            borderRadius: BorderRadius.circular(15.sp),
-          ),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CustomItemFromOPT(),
-            ],
-          ),
-        ),
+        const FinalShapeFromOPT(),
         SizedBox(height: 40.sp),
         const CustomTextFromTextField(
           hintText: 'Password',
@@ -46,5 +31,3 @@ class SectionTextFieldFromResetPassword extends StatelessWidget {
     );
   }
 }
-
-
