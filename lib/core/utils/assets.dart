8c6c9fa52e5b57c012a976e6611ba05a2ed806c2
mohamed <mezoattalla123@gmail.com>
2024-04-02 16:49:48 +0200

    Added a new image asset for congratulations screen.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -4,5 +4,6 @@ abstract class Assets {
   static const String splash = "assets/images/splachImage.png";
   static const String onBoarding = "assets/images/onBoarding.jpg";
   static const String background = "assets/images/background.jpg";
+  static const String congratulation = "assets/images/congratulation.png";
 
 }
