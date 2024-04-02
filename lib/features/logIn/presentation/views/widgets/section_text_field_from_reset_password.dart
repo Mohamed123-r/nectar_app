045e90946f4fe66150ed1d902a20d50e112cf56a
mohamed <mezoattalla123@gmail.com>
2024-04-02 16:20:11 +0200

    "Refactor reset password text field layout and styling in section_text_field_from_reset_password.dart"

--- lib/features/logIn/presentation/views/widgets/section_text_field_from_reset_password.dart
+++ lib/features/logIn/presentation/views/widgets/section_text_field_from_reset_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/constants.dart';
 import 'package:nectar/core/widgets/custom_text_field.dart';
 import 'package:nectar/core/widgets/custom_text_from_text_field.dart';
 import 'package:nectar/core/widgets/password_text_field.dart';
@@ -15,8 +17,48 @@ class SectionTextFieldFromResetPassword extends StatelessWidget {
         const CustomTextFromTextField(
           hintText: 'Enter the 5 digits code',
         ),
-        const CustomTextField(
-          keyboardType: TextInputType.number,
+        Container(
+          height: 67.sp,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            border: Border.all(color: kPrimaryColor),
+            borderRadius: BorderRadius.circular(15.sp),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  maxLines: 1,
+                  cursorColor: kGreyColor,
+                  onSaved: (value) {},
+                  onChanged: (e) {
+                    if (e.length == 1) {
+                      FocusScope.of(context).nextFocus();
+                    } else {
+                      FocusScope.of(context).previousFocus();
+                    }
+                  },
+                  decoration: InputDecoration(
+                    fillColor: kPrimaryColor,
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: kPrimaryColor),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: kPrimaryColor),
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
         SizedBox(height: 40.sp),
         const CustomTextFromTextField(
