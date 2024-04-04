7537f2482bdcf0a2688ace30df779209e9f67ee6
mohamed <mezoattalla123@gmail.com>
2024-04-04 22:28:35 +0200

    Refactor home view body layout to include a grid view for items 1-8. Updated the structure to improve user experience.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -10,16 +10,39 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return NestedScrollView(
+
       floatHeaderSlivers: true,
       headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
         return <Widget>[
           const CustomSliverAppBar(),
         ];
       },
-      body: const SafeArea(
+      body:  SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.white,
-          body: CustomSearchTextField(),
+          body: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                CustomSearchTextField(),
+                SizedBox(height: 20),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    children: const [
+                      Text('Item 1'),
+                      Text('Item 2'),
+                      Text('Item 3'),
+                      Text('Item 4'),
+                      Text('Item 5'),
+                      Text('Item 6'),
+                      Text('Item 7'),
+                      Text('Item 8'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
