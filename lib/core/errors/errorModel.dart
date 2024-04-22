aa85b4419ef57309ddddb076c8df6a961d54b29b
mohamed <mezoattalla123@gmail.com>
2024-04-22 13:59:18 +0200

    Refactor ErrorModel class constructor and remove unused code.

--- lib/core/errors/errorModel.dart
+++ lib/core/errors/errorModel.dart
@@ -1,14 +1,5 @@
 class ErrorModel {
-  int? status;
-  String? message;
 
-  ErrorModel({this.status, this.message});
-
-  ErrorModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    message = json['message'];
-
-    }
 
 
 }
