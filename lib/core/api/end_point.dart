73ad58ee5b6f5ce38e63e024d2e21befc1a46e56
mohamed <mezoattalla123@gmail.com>
2024-04-24 06:59:54 +0200

    Updated EndPoint class with new login endpoint and added ApiKeys for token, username, password, email, phoneNumber, and confirmPassword.

--- lib/core/api/end_point.dart
+++ lib/core/api/end_point.dart
@@ -1,8 +1,15 @@
 class EndPoint {
   static String baseUrl = "http://nectorteam-001-site1.etempurl.com/api/Account/";
+  static String login = "login";
 
 }
 
 class ApiKeys {
 
+  static String token = "token";
+  static String username = "username";
+  static String password = "password";
+  static String email = "email";
+  static String phoneNumber = "phoneNumber";
+  static String confirmPassword ="confirmPassword";
 }
