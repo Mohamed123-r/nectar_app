cace21f2f93e8e2a64903b59ca99a9772bd10455
mohamed <mezoattalla123@gmail.com>
2024-05-05 05:30:02 +0300

    Refactor product detail view body to include additional product details and add to cart button.

--- lib/features/home/presentation/views/widgets/product_detail_view_body.dart
+++ lib/features/home/presentation/views/widgets/product_detail_view_body.dart
@@ -1,42 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectar/core/utils/assets.dart';
+import 'package:nectar/core/widgets/custom_button.dart';
+import 'package:nectar/features/home/presentation/views/widgets/section_details_product_details.dart';
 import 'custom_carousel_slider.dart';
 import 'section_number_and_price_in_product_details.dart';
 import 'section_product_details.dart';
 
-class ProductDetailViewBody extends StatelessWidget {
+class ProductDetailViewBody extends StatefulWidget {
   const ProductDetailViewBody({
     super.key,
   });
 
   @override
+  State<ProductDetailViewBody> createState() => _ProductDetailViewBodyState();
+}
+
+class _ProductDetailViewBodyState extends State<ProductDetailViewBody> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 350.sp,
-          decoration: const BoxDecoration(
-            color: Color(0xFFF2F3F2),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(40.0),
-              bottomRight: Radius.circular(40.0),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 290.sp,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF2F3F2),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40.0),
+                bottomRight: Radius.circular(40.0),
+              ),
+            ),
+            child: CustomCarouselSlider(
+              height: 280.sp,
+              autoPlay: false,
+              imageSliders: [
+                Image.asset(Assets.iconTest),
+                Image.asset(Assets.iconTest),
+                Image.asset(Assets.iconTest),
+              ],
             ),
           ),
-          child: CustomCarouselSlider(
-            height: 350.sp,
-            autoPlay: false,
-            imageSliders: [
-              Image.asset(Assets.iconTest),
-              Image.asset(Assets.iconTest),
-              Image.asset(Assets.iconTest),
-            ],
+          const SectionProductDetails(),
+          const SectionNumberAndPriceInProductDetails(),
+          const SectionDetailsProductDetails(),
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 16.0.w),
+            child: CustomButton(
+              text: "Add to cart",
+              onPressed: () {},
+            ),
           ),
-        ),
-        const SectionProductDetails(),
-        const SectionNumberAndPriceInProductDetails(),
-      ],
+        ],
+      ),
     );
   }
 }
