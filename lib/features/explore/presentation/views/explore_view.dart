abc47a989593dfc98250e05e6b1d29068db856c5
mohamed <mezoattalla123@gmail.com>
2024-05-06 15:12:30 +0300

    Refactor explore_view.dart and explore_view_body.dart for better code organization and readability.

--- lib/features/explore/presentation/views/explore_view.dart
+++ lib/features/explore/presentation/views/explore_view.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nectar/core/utils/assets.dart';
 import 'package:nectar/core/utils/styles.dart';
-import 'package:nectar/core/widgets/custom_text_field_search.dart';
-import 'package:nectar/features/explore/presentation/views/widgets/explore_view_body.dart';
+import 'widgets/explore_view_body.dart';
 
 class ExploreView extends StatelessWidget {
   const ExploreView({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +15,7 @@ class ExploreView extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body:  ExploreViewBody(),
+      body: ExploreViewBody(),
     );
   }
 }
--- lib/features/explore/presentation/views/widgets/explore_view_body.dart
+++ lib/features/explore/presentation/views/widgets/explore_view_body.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nectar/core/utils/assets.dart';
-import 'package:nectar/core/utils/styles.dart';
 import 'package:nectar/core/widgets/custom_text_field_search.dart';
+import 'grid_view_item.dart';
 
 class ExploreViewBody extends StatelessWidget {
-   ExploreViewBody({super.key});
+  ExploreViewBody({super.key});
 
   final List<Color> backgroundColors = [
     const Color(0xFF53B175),
@@ -43,37 +42,9 @@ class ExploreViewBody extends StatelessWidget {
               mainAxisSpacing: 15.w,
               children: List.generate(
                 15,
-                (index) => Container(
-                  height: 190.h,
-                  width: 175.w,
-                  decoration: BoxDecoration(
-                    color: backgroundColors[index].withOpacity(.20),
-                    borderRadius: BorderRadius.circular(18.sp),
-                    border: Border.all(
-                      color: backgroundColors[index],
-                      width: 1,
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        Assets.proTest,
-                        height: 75.h,
-                        width: 111,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      const Text(
-                        "Fresh Fruits & Vegetable",
-                        textAlign: TextAlign.center,
-                        style: Styles.textStyle16,
-                      )
-                    ],
-                  ),
+                (index) => GridViewItem(
+                  backgroundColors: backgroundColors,
+                  index: index,
                 ),
               ),
             ),
