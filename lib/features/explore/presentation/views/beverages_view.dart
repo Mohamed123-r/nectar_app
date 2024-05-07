2d132576afcb0c25fbb8d75140a89aadf695f21d
mohamed <mezoattalla123@gmail.com>
2024-05-07 22:39:59 +0300

    Refactor explore_view.dart and explore_view_body.dart for better code organization and readability.

---
+++ lib/features/explore/presentation/views/beverages_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BeveragesView extends StatelessWidget {
+  const BeveragesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
