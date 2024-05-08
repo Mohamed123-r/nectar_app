f1294b6b5e798da4099238b80b8675ef0ac7c91c
mohamed <mezoattalla123@gmail.com>
2024-05-08 21:51:01 +0300

    Commit: Added BeveragesViewBody widget to BeveragesView Repository: D:/ProjectFlutter/nectar

--- lib/features/explore/presentation/views/beverages_view.dart
+++ lib/features/explore/presentation/views/beverages_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/beverages_view_body.dart';
+
 class BeveragesView extends StatelessWidget {
   const BeveragesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const BeveragesViewBod();
   }
 }
---
+++ lib/features/explore/presentation/views/widgets/beverages_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BeveragesViewBod extends StatelessWidget {
+  const BeveragesViewBod({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
