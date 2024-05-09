45651ceadae136cc18b459bf675e7914f0ac8f2b
mohamed <mezoattalla123@gmail.com>
2024-05-09 16:42:34 +0300

    Added new import statements and implemented a GridView with CategoryItem widgets in the SearchViewBody widget.

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/widgets/category_item.dart';
 import 'search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -19,7 +21,19 @@ class SearchViewBody extends StatelessWidget {
           ),
         ],
       ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.h),
+        child: GridView.count(
+          mainAxisSpacing: 10.h,
+          crossAxisSpacing: 15.h,
+          childAspectRatio: 0.72,
+          crossAxisCount: 2,
+          children: List.generate(
+            15,
+            (index) => const CategoryItem(),
+          ),
+        ),
+      ),
     );
   }
 }
-
