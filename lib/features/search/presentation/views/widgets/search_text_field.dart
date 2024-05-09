a621875055ff4b662ca083fdd6366738bf090d20
mohamed <mezoattalla123@gmail.com>
2024-05-09 16:21:46 +0300

    Refactor search view body to include a custom search text field widget and tune icon in app bar. Add search text field widget for better user experience.

---
+++ lib/features/search/presentation/views/widgets/search_text_field.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:nectar/constants.dart';
+import 'package:nectar/core/utils/styles.dart';
+
+class SearchTextField extends StatefulWidget {
+  const SearchTextField({
+    super.key,
+  });
+
+  @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  bool hasFocus = false;
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textController,
+      cursorColor: kPrimaryColor,
+      keyboardType: TextInputType.text,
+      style: Styles.textStyle16,
+      onChanged: (value) {
+        setState(() {
+          hasFocus = value.isNotEmpty;
+        });
+      },
+      decoration: InputDecoration(
+        prefixIcon: const Icon(
+          Icons.search,
+          color: Colors.black,
+        ),
+        suffixIcon: Builder(
+          builder: (context) {
+            if (hasFocus) {
+              return IconButton(
+                onPressed: () {
+                  _textController.clear();
+                  setState(() {
+                    hasFocus = false;
+                  });
+                },
+                icon: Icon(
+                  Icons.cancel,
+                  color: Colors.grey[400],
+                ),
+              );
+            } else {
+              return const SizedBox();
+            }
+          },
+        ),
+        hintText: 'Search',
+        hintStyle: const TextStyle(color: Colors.black),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+        filled: true,
+        fillColor: Colors.grey[300],
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: const BorderSide(
+              color: Colors.white,
+            )),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: const BorderSide(
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: SearchTextField(),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.tune,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
