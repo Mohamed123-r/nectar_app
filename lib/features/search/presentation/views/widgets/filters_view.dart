95a7ee2adc8b8c82fe00ca8ae5a2050df3b5c77a
mohamed <mezoattalla123@gmail.com>
2024-05-10 15:03:32 +0300

    Added FiltersView widget to the search feature presentation views.

---
+++ lib/features/search/presentation/views/widgets/filters_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FiltersView extends StatelessWidget {
+  const FiltersView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
