6e69977322c4ae8768f4532cb3707350b82934e5
mohamed <mezoattalla123@gmail.com>
2024-05-11 22:56:51 +0300

    Refactor filters view to use separate body widget for better organization and readability.

--- lib/features/search/presentation/views/filters_view.dart
+++ lib/features/search/presentation/views/filters_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/filters_view_body.dart';
+
 class FiltersView extends StatelessWidget {
   const FiltersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const FiltersViewBody();
   }
 }
+
+
+
