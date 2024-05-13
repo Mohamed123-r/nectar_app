368cfa7e9101be90a6a23b74f2b67938942ff632
mohamed <mezoattalla123@gmail.com>
2024-05-13 21:58:01 +0300

    Updated main.dart to use CartView instead of SearchView as the home screen, and added CartView and MyCartBody to the project.

---
+++ lib/features/myCart/presentation/views/cart_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:nectar/core/utils/styles.dart';
+import 'widgets/my_cart_body.dart';
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "My Cart",
+          style: Styles.textStyle20,
+        ),
+      ),
+      body: const MyCartBody(),
+    );
+  }
+}
---
+++ lib/features/myCart/presentation/views/widgets/my_cart_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCartBody  extends StatelessWidget {
+  const MyCartBody ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
