3d19c2c2f6d5d6321642cc9307e56dafc9047954
mohamed <mezoattalla123@gmail.com>
2024-05-13 22:14:00 +0300

    Updated myCartBody widget to display an image and text in a row, with proper sizing using Flutter ScreenUtil.

--- lib/features/myCart/presentation/views/widgets/my_cart_body.dart
+++ lib/features/myCart/presentation/views/widgets/my_cart_body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/utils/assets.dart';
 
 class MyCartBody  extends StatelessWidget {
   const MyCartBody ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Center(child: Row(
+      children: [
+        Image.asset(
+          Assets.iconTest,
+          width: 125.sp,
+          height: 80.sp,
+        ),
+        const Column(
+          children: [
+            Text(""),
+          ],
+        )
+      ],
+    ),);
   }
 }
