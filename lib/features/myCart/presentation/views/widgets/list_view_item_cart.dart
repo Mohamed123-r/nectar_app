fa524904e48d007a7ebab8cbb7f44da75ce1e6e5
mohamed <mezoattalla123@gmail.com>
2024-05-18 16:56:14 +0300

    Added new file `list_view_item_cart.dart` in the `myCart` feature module to display individual item details in the cart view. Includes image, item name, quantity, and price with an option to remove the item.

---
+++ lib/features/myCart/presentation/views/widgets/list_view_item_cart.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/constants.dart';
+import 'package:nectar/core/utils/assets.dart';
+import 'package:nectar/core/utils/styles.dart';
+import 'package:nectar/features/myCart/presentation/views/widgets/section_number_and_price_in_cart_view.dart';
+
+class ListViewItemCart extends StatelessWidget {
+  const ListViewItemCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.asset(
+            Assets.iconTest,
+            fit: BoxFit.fill,
+            width: 85.sp,
+            height: 85.sp,
+          ),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Bell Pepper Red",
+                    style: Styles.textStyle16,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(
+                      Icons.close,
+                      color: kGreyColor,
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                "1kg Price",
+                style: Styles.textStyle14,
+              ),
+              const SectionNumberAndPriceInCartView()
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
