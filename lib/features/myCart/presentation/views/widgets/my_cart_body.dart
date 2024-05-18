b7ac36c94bd0d4ec20ba433a4a2391182c9d9750
mohamed <mezoattalla123@gmail.com>
2024-05-18 17:06:57 +0300

    Updated MyCartBody widget to include a "Go to Checkout" button and additional imports for better functionality.

--- lib/features/myCart/presentation/views/widgets/my_cart_body.dart
+++ lib/features/myCart/presentation/views/widgets/my_cart_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/widgets/custom_button.dart';
 import 'list_view_item_cart.dart';
 
 class MyCartBody extends StatelessWidget {
@@ -7,13 +8,27 @@ class MyCartBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 20,
-      itemBuilder: (BuildContext context, int index) => Padding(
-        padding: EdgeInsets.symmetric(vertical: 16.0.sp),
-        child: const ListViewItemCart(),
+    return Stack(children: [
+      ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) => Padding(
+          padding: index == 19
+              ? EdgeInsets.only(top: 16.sp, bottom: 80.sp)
+              : EdgeInsets.symmetric(vertical: 16.0.sp),
+          child: const ListViewItemCart(),
+        ),
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
+      Positioned(
+        bottom: 10,
+        left: 0,
+        right: 0,
+        child: CustomButton(
+          text: 'Go to Checkout',
+          onPressed: () {},
+        ),
+      ),
+    ]);
   }
 }
