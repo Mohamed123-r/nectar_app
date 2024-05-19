fb6ebb69f12c85a3fcac9fee507cb4a24e8670e6
mohamed <mezoattalla123@gmail.com>
2024-05-19 14:05:03 +0300

    Added a new file for the FavoritesViewBody widget with a ListView and CustomButton for adding all items to the cart.

---
+++ lib/features/favorites/presentation/views/widgets/favourite_view_body.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/widgets/custom_button.dart';
+
+import 'list_view_item_favourite.dart';
+
+class FavoritesViewBody extends StatelessWidget {
+  const FavoritesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) => Padding(
+          padding: index == 19
+              ? EdgeInsets.only(top: 16.sp, bottom: 80.sp)
+              : EdgeInsets.symmetric(vertical: 16.0.sp),
+          child: const ListViewItemFavourite(),
+        ),
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
+      Positioned(
+        bottom: 10.h,
+        left: 0.h,
+        right: 0.h,
+        child: CustomButton(
+          text: 'Add all to Cart',
+          onPressed: () {},
+        ),
+      ),
+    ]);
+  }
+}
