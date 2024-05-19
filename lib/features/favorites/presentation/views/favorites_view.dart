af51c5b79ac9a9eb4ef0f807b343e26230736a1d
mohamed <mezoattalla123@gmail.com>
2024-05-19 14:05:12 +0300

    Updated FavoritesView to include an app bar with a title and specified border color and width. Added body padding for better styling.

--- lib/features/favorites/presentation/views/favorites_view.dart
+++ lib/features/favorites/presentation/views/favorites_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:nectar/constants.dart';
+import 'package:nectar/core/utils/styles.dart';
+
+import 'widgets/favourite_view_body.dart';
 
 class FavoritesView extends StatelessWidget {
-  const FavoritesView ({super.key});
+  const FavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        shape: const Border(
+          bottom: BorderSide(
+            color: kGreyColor,
+            width: 0.5,
+          ),
+        ),
+        title: const Text(
+          'Favourite',
+          style: Styles.textStyle20,
+        ),
+        centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: FavoritesViewBody(),
+      ),
+    );
   }
 }
+
