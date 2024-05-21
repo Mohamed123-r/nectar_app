1a5f7960136f34e67e1265a8bc1df4ca116b0173
mohamed <mezoattalla123@gmail.com>
2024-05-21 20:08:07 +0300

    Refactored showModalBottomSheet for checkout functionality in myCartBody widget

--- lib/features/myCart/presentation/views/widgets/my_cart_body.dart
+++ lib/features/myCart/presentation/views/widgets/my_cart_body.dart
@@ -29,87 +29,71 @@ class MyCartBody extends StatelessWidget {
         child: CustomButton(
           text: 'Go to Checkout',
           onPressed: () {
-            showBottomSheet(
+            showModalBottomSheet(
+              backgroundColor: Colors.white,
               context: context,
-              builder: (context) {
-                return BottomSheet(
-                  backgroundColor: Colors.white,
-                  enableDrag: true,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.sp),
-                      topRight: Radius.circular(20.sp),
-                    ),
-                  ),
-                  onClosing: () {},
-                  builder: (context) {
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
+              builder: (context) => Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 16.0.w, vertical: 24.h),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 16.0.w, vertical: 24.h),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text(
-                                "Checkout",
-                                style: Styles.textStyle20,
-                              ),
-                              GestureDetector(
-                                  onTap: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Icon(Icons.close)),
-                            ],
-                          ),
+                        const Text(
+                          "Checkout",
+                          style: Styles.textStyle20,
                         ),
-                        const Divider(),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: ExpansionPanelList(
-                            elevation: 0,
-                            expandIconColor: Colors.black,
-                            children: [
-                              ExpansionPanel(
-                                backgroundColor: Colors.white,
-                                headerBuilder: (context, isExpanded) {
-                                  return Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        "Delivery",
-                                        style: Styles.textStyle18.copyWith(
-                                          color: kGreyColor,
-                                        ),
-                                      ),
-                                      const Text("Select Method",
-                                          style: Styles.textStyle16),
-                                    ],
-                                  );
-                                },
-                                body: const Text("Item 1"),
-                                isExpanded: false,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: CustomButton(
-                            text: 'Checkout',
-                            onPressed: () {
+                        GestureDetector(
+                            onTap: () {
                               Navigator.pop(context);
                             },
-                          ),
-                        )
+                            child: const Icon(Icons.close)),
                       ],
-                    );
-                  },
-                );
-              },
+                    ),
+                  ),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: ExpansionPanelList(
+                      elevation: 0,
+                      expandIconColor: Colors.black,
+                      children: [
+                        ExpansionPanel(
+                          backgroundColor: Colors.white,
+                          headerBuilder: (context, isExpanded) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Delivery",
+                                  style: Styles.textStyle18.copyWith(
+                                    color: kGreyColor,
+                                  ),
+                                ),
+                                const Text("Select Method",
+                                    style: Styles.textStyle16),
+                              ],
+                            );
+                          },
+                          body: const Text("Item 1"),
+                          isExpanded: false,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CustomButton(
+                      text: 'Checkout',
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  )
+                ],
+              ),
             );
           },
         ),
