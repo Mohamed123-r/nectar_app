d308da57b399116d374fa6eecb1dd75499abd5f1
mohamed <mezoattalla123@gmail.com>
2024-05-25 20:29:37 +0300

    Implemented AccountViewBody widget and updated the AccountView with AppBar and body.

--- lib/features/account/presentation/views/account_view.dart
+++ lib/features/account/presentation/views/account_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nectar/constants.dart';
+import 'package:nectar/core/utils/styles.dart';
+import 'widgets/account_view_body.dart';
 
 class AccountView extends StatelessWidget {
   const AccountView ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        shape: const Border(
+          bottom: BorderSide(
+            color: kGreyColor,
+            width: 0.5,
+          ),
+        ),
+        centerTitle: true,
+        title: const Text(
+          "Account",
+          style: Styles.textStyle20,
+        ),
+      ),
+    body: const AccountViewBody(),
+    );
   }
 }
---
+++ lib/features/account/presentation/views/widgets/account_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountViewBody extends StatelessWidget {
+  const AccountViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
