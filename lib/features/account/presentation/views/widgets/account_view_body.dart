064250748e8ab39a6886ca80cfa769b8a7e86a6e
mohamed <mezoattalla123@gmail.com>
2024-05-26 14:46:52 +0300

    Refactor account view body widget to include user profile details and navigation links.

--- lib/features/account/presentation/views/widgets/account_view_body.dart
+++ lib/features/account/presentation/views/widgets/account_view_body.dart
@@ -10,54 +10,157 @@ class AccountViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 16.0.sp,
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding:  EdgeInsets.symmetric(vertical: 24.0.h),
-              child: Row(
-                children: [
-                  Container(
-                    height: 65.h,
-                    width: 65.w,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          Assets.accontTest,
-                        ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 24.0.h),
+            child: Row(
+              children: [
+                Container(
+                  height: 65.h,
+                  width: 65.w,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        Assets.accontTest,
                       ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          27,
-                        ),
-                      ),
-                      color: Colors.blue,
                     ),
-                  ),
-                  SizedBox(
-                    width: 20.w,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "John Doe",
-                        style: Styles.textStyle20,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        27,
                       ),
-                      Text("Imshuvo97@gmail.com",
-                          style: Styles.textStyle16.copyWith(
-                            color: kGreyColor,
-                          ))
-                    ],
+                    ),
+                    color: Colors.blue,
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  width: 20.w,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "John Doe",
+                      style: Styles.textStyle20,
+                    ),
+                    Text("Imshuvo97@gmail.com",
+                        style: Styles.textStyle16.copyWith(
+                          color: kGreyColor,
+                        ))
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.shopping_bag_outlined,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Orders',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+
+              Icons.badge_outlined,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'My Details',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+
+              Icons.location_on_outlined,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Delivery Address',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.credit_card,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Payment Methods',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+             Icons.local_offer_outlined,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Promo Cord',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.notifications_none,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Notifications',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.help_outline,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'Help',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.error_outline,
+              size: 27.sp,
+            ),
+            title: const Text(
+              'About',
+              style: Styles.textStyle18,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+          const Divider(),
+
+        ],
       ),
     );
   }
