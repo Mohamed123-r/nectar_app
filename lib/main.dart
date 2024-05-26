c959ca483476395e2267e25d2523afbbe9646f01
mohamed <mezoattalla123@gmail.com>
2024-05-26 14:17:54 +0300

    Added system overlay style configuration for navigation and status bar colors in the main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectar/core/database/cache/cache_helper.dart';
 import 'package:nectar/features/account/presentation/views/account_view.dart';
+
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper().init();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent, // navigation bar color
+      statusBarColor: Colors.transparent, // status bar color
+    ),
+  );
+
   runApp(
     const MyApp(),
   );
