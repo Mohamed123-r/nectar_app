010279e67c760815d308fdb84e524eedd5161db8
mohamed <mezoattalla123@gmail.com>
2024-05-27 21:42:52 +0300

    Refactor home_view_body.dart to convert StatefulWidget to StatelessWidget and remove unused imports and code.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nectar/constants.dart';
 import 'package:nectar/core/utils/assets.dart';
 import 'package:nectar/features/home/presentation/views/widgets/section_best_selling.dart';
 import 'package:nectar/features/home/presentation/views/widgets/section_exclusive_offer.dart';
@@ -11,25 +10,12 @@ import 'section_groceries.dart';
 import 'sliver_appbar.dart';
 import 'title_groups_items.dart';
 
-class HomeViewBody extends StatefulWidget {
+class HomeViewBody extends StatelessWidget {
   const HomeViewBody({
     super.key,
   });
 
   @override
-  State<HomeViewBody> createState() => _HomeViewBodyState();
-}
-
-class _HomeViewBodyState extends State<HomeViewBody> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-
   Widget build(BuildContext context) {
     return SafeArea(
       child: NestedScrollView(
@@ -39,34 +25,6 @@ class _HomeViewBodyState extends State<HomeViewBody> {
           ];
         },
         body: Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.storefront),
-                label: 'Shop',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.manage_search),
-                label: 'Explore',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
-                label: 'Card',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border),
-                label: 'Favorite',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline),
-                label: 'Account',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: kPrimaryColor,
-            unselectedItemColor: const Color(0xff181725),
-            onTap: _onItemTapped,
-          ),
           body: Column(
             children: [
               const CustomSearchTextField(),
