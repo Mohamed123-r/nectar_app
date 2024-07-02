584a297ff362e28c7d4aca316e9e395248cf15ac
mohamed <mezoattalla123@gmail.com>
2024-07-03 02:35:51 +0300

    Update ApiKeys in end_point.dart to include statusCode, isSuccess, message, data, and id fields.

--- lib/core/api/end_point.dart
+++ lib/core/api/end_point.dart
@@ -1,15 +1,17 @@
 class EndPoint {
-  static String baseUrl =
-      "http://ahmedsalah.runasp.net/api/Account/";
+  static String baseUrl = "http://ahmedsalah.runasp.net/api/Account/";
 
   static String login = "login";
 }
 
 class ApiKeys {
-  static String token = "token";
+  static String statusCode = "statusCode";
+  static String isSuccess = "isSuccess";
+  static String message = "message";
+  static String data = "data";
   static String username = "username";
-  static String password = "password";
   static String email = "email";
-  static String phoneNumber = "phoneNumber";
-  static String confirmPassword = "confirmPassword";
+  static String password = "password";
+  static String id = "id";
+  static String token = "token";
 }
