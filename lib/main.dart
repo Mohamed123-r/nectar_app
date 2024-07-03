3a2f7d59ae2be2e91169b09230bf929a46ae47d9
mohamed <mezoattalla123@gmail.com>
2024-07-04 02:04:05 +0300

    Remove unused imports from main.dart for cleaner code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nectar/core/api/dio_consumer.dart';
 import 'package:nectar/core/database/cache/cache_helper.dart';
 import 'package:nectar/features/splash/presentation/view/splash_view.dart';
 
-import 'features/logIn/presentation/manager/cubits/log_in_cubit.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
