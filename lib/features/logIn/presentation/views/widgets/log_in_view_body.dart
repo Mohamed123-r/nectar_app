0ad1dc8ed9231cf6cf0903564f6ac3edae590166
mohamed <mezoattalla123@gmail.com>
2024-07-03 05:18:33 +0300

    Enhance error dialog in LogInViewBody with additional actions and improved styling.

--- lib/features/logIn/presentation/views/widgets/log_in_view_body.dart
+++ lib/features/logIn/presentation/views/widgets/log_in_view_body.dart
@@ -37,12 +37,38 @@ class _LogInViewBodyState extends State<LogInViewBody> {
         } else if (state is LogInError) {
           AwesomeDialog(
             context: context,
-            dialogType: DialogType.error,
+            dialogType: DialogType.warning,
             headerAnimationLoop: false,
             animType: AnimType.scale,
             title: 'Oops!',
             desc: state.message,
-            btnOkOnPress: () {},
+            descTextStyle: Styles.textStyle14.copyWith(color: Colors.black),
+            btnOk: Column(
+              children: [
+                CustomButton(
+                  text: 'Please try again',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                SizedBox(height: 10.sp),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        AppRouter.router(
+                          const RouteSettings(
+                            name: AppRouter.kSignUpView,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Go to Sign Up',
+                      style: Styles.textStyle18.copyWith(color: kPrimaryColor),
+                    )),
+              ],
+            ),
           ).show();
         }
       },
