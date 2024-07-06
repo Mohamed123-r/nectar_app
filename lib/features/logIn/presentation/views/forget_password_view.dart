4ac42349ad4ddeac43acabf4a5a62b44850d5e4f
mohamed <mezoattalla123@gmail.com>
2024-07-06 06:07:39 +0300

    Refactor ForgotPasswordView to use ForgetPasswordBody and integrate SendNumCubit for email verification process

--- lib/features/logIn/presentation/views/forget_password_view.dart
+++ lib/features/logIn/presentation/views/forget_password_view.dart
@@ -1,5 +1,13 @@
+import 'dart:developer';
+
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/constants.dart';
+import 'package:nectar/core/api/dio_consumer.dart';
+import 'package:nectar/core/function/validator.dart';
 import 'package:nectar/core/utils/app_router.dart';
 import 'package:nectar/core/utils/assets.dart';
 import 'package:nectar/core/utils/styles.dart';
@@ -7,97 +15,23 @@ import 'package:nectar/core/widgets/custom_button.dart';
 import 'package:nectar/core/widgets/custom_image_logo.dart';
 import 'package:nectar/core/widgets/custom_text_field.dart';
 import 'package:nectar/core/widgets/custom_text_from_text_field.dart';
+import 'package:nectar/features/logIn/presentation/manager/cubits/send_num_cubit.dart';
 import 'package:nectar/features/logIn/presentation/views/widgets/bottom_sheet_body_from_send_code.dart';
+import 'package:nectar/features/logIn/presentation/views/widgets/forget_password_body.dart';
+
+import '../manager/cubits/send_num_state.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(Assets.background),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0.sp),
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    SizedBox(height: 70.sp),
-                    const CustomImageLogo(),
-                    const SizedBox(height: 100),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Forgot Password",
-                          style: Styles.textStyle26,
-                        ),
-                        SizedBox(height: 15.sp),
-                        const CustomTextFromTextField(
-                            hintText: "Enter your email for verification"
-                                " process we will send 6 digits code"
-                                " to your email"),
-                        SizedBox(height: 40.sp),
-                        const CustomTextFromTextField(
-                          hintText: 'Email',
-                        ),
-                        CustomTextField(
-                          onSaved: (value) {},
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 60.sp),
-                    CustomButton(
-                      text: "Continue",
-                      onPressed: () {
-                        showModalBottomSheet(
-                          isScrollControlled: true,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30.sp),
-                              topRight: Radius.circular(30.sp),
-                            ),
-                          ),
-                          context: context,
-                          builder: (context) {
-                            return const BottomSheetBodyFromSendCode();
-                          },
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                Positioned(
-                  top: 20,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        AppRouter.router(
-                          const RouteSettings(name: AppRouter.kLogInView),
-                        ),
-                      );
-                    },
-                    icon: Icon(
-                      Icons.arrow_back_ios,
-                      size: 25.sp,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+    return BlocProvider(
+      create: (context) => SendNumCubit(
+        DioConsumer(dio: Dio()),
       ),
+      child: const ForgetPasswordBody(),
     );
   }
 }
+
---
+++ lib/features/logIn/presentation/views/widgets/forget_password_body.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectar/core/widgets/custom_image_logo.dart';
+import 'package:nectar/core/widgets/custom_text_from_text_field.dart';
+
+import '../../../../../constants.dart';
+import '../../../../../core/function/awesome_dialog.dart';
+import '../../../../../core/function/validator.dart';
+import '../../../../../core/utils/app_router.dart';
+import '../../../../../core/utils/assets.dart';
+import '../../../../../core/utils/styles.dart';
+import '../../../../../core/widgets/custom_button.dart';
+import '../../../../../core/widgets/custom_text_field.dart';
+import '../../manager/cubits/send_num_cubit.dart';
+import '../../manager/cubits/send_num_state.dart';
+import '../widgets/bottom_sheet_body_from_send_code.dart';
+
+import 'package:awesome_dialog/awesome_dialog.dart';
+
+class ForgetPasswordBody extends StatelessWidget {
+  const ForgetPasswordBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SendNumCubit, SendNumState>(
+      listener: (context, state) {
+        if (state is SendNumSuccess) {
+          showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.sp),
+                  topRight: Radius.circular(30.sp),
+                ),
+              ),
+              context: context,
+              builder: (context) {
+                return const BottomSheetBodyFromSendCode();
+              });
+        } else if (state is SendNumError) {
+          awesomeDialogFromSendCode(context, state);
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.background),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0.sp),
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(height: 70.sp),
+                        const CustomImageLogo(),
+                        const SizedBox(height: 100),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Forgot Password",
+                              style: Styles.textStyle26,
+                            ),
+                            SizedBox(height: 15.sp),
+                            const CustomTextFromTextField(
+                                hintText: "Enter your email for verification"
+                                    " process we will send 6 digits code"
+                                    " to your email"),
+                            SizedBox(height: 40.sp),
+                            const CustomTextFromTextField(
+                              hintText: 'Email',
+                            ),
+                            CustomTextField(
+                              onSaved: (value) {},
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value) {
+                                return validatorOfEmail(value);
+                              },
+                              controller:
+                                  context.read<SendNumCubit>().sendNumEmail,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 60.sp),
+                        state is SendNumLoading
+                            ? const CircularProgressIndicator(
+                                color: kPrimaryColor,
+                              )
+                            : CustomButton(
+                                text: "Continue",
+                                onPressed: () {
+                                  context
+                                          .read<SendNumCubit>()
+                                          .sendNumEmail
+                                          .text
+                                          .contains(RegExp(
+                                              r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))
+                                      ? context.read<SendNumCubit>().sendNum()
+                                      : awesomeDialogFromSendCode(context, state);
+                                },
+                              ),
+                      ],
+                    ),
+                    Positioned(
+                      top: 20,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            AppRouter.router(
+                              const RouteSettings(name: AppRouter.kLogInView),
+                            ),
+                          );
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          size: 25.sp,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Future<dynamic> awesomeDialogFromSendCode(BuildContext context, dynamic state,
+    {bool isLogin = false}) {
+  return AwesomeDialog(
+    context: context,
+    dialogType: DialogType.error,
+    animType: AnimType.bottomSlide,
+    title: 'Oops, Error',
+    desc: 'Please enter a valid email',
+    descTextStyle: Styles.textStyle14.copyWith(color: Colors.black),
+    btnOk: CustomButton(
+      text: 'Please try again',
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    ),
+  ).show();
+}
